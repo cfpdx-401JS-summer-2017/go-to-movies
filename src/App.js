9cd984a4d71db21e0da7f8559b82131db8a8307f
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-08-16 10:22:39 -0700

    Only show movies if there

--- src/App.js
+++ src/App.js
@@ -6,63 +6,100 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      movies: null,
-      page: 1,
-      loading: true
-    };
-  }
+        this.state = {
+            movies: null,
+            page: 1,
+            loading: true,
+            value: ''
+        };
+    }
 
-  componentDidMount() {
-    this.fetchMovies(this.state.page);
-  }
 
-  fetchMovies(page) {
-    const title = 'something';
-    this.setState({
-      movies: []
-    });
+    handleChange(event) {
+        this.setState({
+            value: event.target.value
+        });
+    }
 
-  fetch(`http://www.omdbapi.com/?s=${encodeURI(title)}&plot=short&r=json&page=${page}&apikey=${API_KEY}`)
-    .then(res => res.json())
-    .then(data => data.Search)
-    .then(movies => {
-      this.setState({
-        movies,
-        loading: false
-      });
-    });
-  }
+    handleSubmit(event) {
+        console.log('submitted was', this.state.value);
+        this.fetchMovies(this.state.value);
+        event.preventDefault();
+    }
 
-  handlePageChange(incr) {
-    const page = Math.max(1, this.state.page + incr);
-    this.setState({ page });
-    this.fetchMovies(page);
-  }
+    // handleSearchInput(value) {
+    //     console.log('inside handlesearchinput value is', value, 'this.state.title is', this.state.title);
+    //     this.setState({
+    //         title: value
+    //     });
+    // }
 
-  render() {
-    const { loading, movies } = this.state;
-    if(loading) return <div>Loading...</div>;
+    componentDidMount() {
+        // this.handleSearchInput(this.state.title);
+        this.fetchMovies(this.state.page);
+    }
 
-    return (
-      <div>
-        <PagingButton label="Prev Page" incr={-1} onClick={this.handlePageChange.bind(this)} />
-        <PagingButton label="Next Page" incr={1} onClick={this.handlePageChange.bind(this)} />
-        <Movies movies={movies} />
-      </div>
-    );
-  }
+    fetchMovies(page) {
+        const title = `${this.state.value}`;
+        console.log('title is', title);
+        this.setState({
+            movies: []
+        });
+
+        fetch(`http://www.omdbapi.com/?s=${encodeURI({title})}&plot=short&r=json&page=${page}&apikey=${API_KEY}`)
+            .then(res => res.json())
+            .then(data => data.Search)
+            .then(movies => {
+                console.log('movies are', movies);
+                if(movies) {
+                    this.setState({
+                        movies,
+                        loading: false
+                    });
+                }
+            });
+    }
+
+    handlePageChange(incr) {
+        const page = Math.max(1, this.state.page + incr);
+        this.setState({ page });
+        this.fetchMovies(page);
+    }
+
+    render() {
+        const { loading, movies } = this.state;
+        if(loading) return <div>Loading...</div>;
+
+        return (
+            <div>
+                <div>
+                    <PagingButton label="Prev Page" incr={-1} onClick={this.handlePageChange.bind(this)} />
+                    <PagingButton label="Next Page" incr={1} onClick={this.handlePageChange.bind(this)} />
+                    <div>
+                        <form onSubmit={this.handleSubmit.bind(this)}>
+                            <label>
+                                Search by Title:
+                                <input type="text" name="title" style={{ width: '300px' }} value={this.state.value} onChange={this.handleChange.bind(this)} />
+                            </label>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                    <Movies movies={movies} />
+                </div>
+            </div>
+        );
+    }
 }
 
 function PagingButton({ onClick, incr, label }) {
-  return (
-    <button onClick={() => onClick(incr)}>
-      {label}
-    </button>
-  );
+    return (
+        <button onClick={() => onClick(incr)}>
+            {label}
+        </button>
+    );
 }
 
 export default App;
