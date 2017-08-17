1929c0da8cf0900d2fd5f413402b47725678c700
Christopher Thomson <christopher.thomson42@gmail.com>
2017-08-17 10:47:44 -0700

    Fixed unmutatible search field

--- src/App.js
+++ src/App.js
@@ -9,20 +9,21 @@ class App extends Component {
 
     this.state = {
       movies: null,
-      title: "star",
+      title: '',
       loading: true
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleChange(target) {
     this.setState({
-      value: event.target.value
+      title: target.value
     });
   }
 
   handleSubmit(event) {
+    event.preventDefault()
     console.log(this.state.title);
     this.fetchMovies(this.state.title);
   }
@@ -55,7 +56,8 @@ class App extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Search..." value={this.state.title} onChange={this.handleChange}/>
+          <input type="text" placeholder="Search..." value={this.state.title} 
+          onChange={({ target }) => this.handleChange(target)}/>
           <input type="submit" value="Submit"/>
         </form>
       </div>
