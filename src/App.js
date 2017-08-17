19e0d5746f55d293f72a4d57325c2fe4c817ab98
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-08-17 13:15:07 -0700

    the paging and the title are now showing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Movies} from './movies/Movies';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 class App extends Component {
@@ -9,33 +10,47 @@ class App extends Component {
 
     this.state = {
       movies: null,
-      title: "star",
+      title: '',
+      page: 1, 
+      // poster: " ",
+      // search: search,
       loading: true
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleChange(event) {
+  handleChange(target) {
     this.setState({
-      value: event.target.value
+      title: target.value
     });
   }
 
+  handlePageChange(incr) {
+    const page = Math.max(1, this.state.page + incr);
+    this.setState({ page });
+    this.fetchMovies(this.state.title, page);
+  }
+  handleInputChange(event) {
+    this.setState({title:event.target.value})
+  }
+
   handleSubmit(event) {
+    event.preventDefault()
     console.log(this.state.title);
     this.fetchMovies(this.state.title);
   }
   componentDidMount() {
-    this.fetchMovies(this.state.title);
+    this.fetchMovies(this.state.title, this.state.page);
   }
-  fetchMovies(title) {
-    // const title = '';
+  fetchMovies(title, page) {
+    // const title: 'St';
     this.setState({
       movies:[]
     });
-
-  fetch(`http://www.omdbapi.com/?t=${encodeURI(title)}&plot=full&apikey=${API_KEY}`)
+console.log(encodeURI(title));
+  fetch(`http://www.omdbapi.com/?s=${encodeURI(title)}&plot=full&page=${page}&apikey=${API_KEY}`)
     .then(res => res.json())
     .then(data => data.Search)
     .then(movies => {
@@ -50,17 +65,43 @@ class App extends Component {
 
   render() {
     const { loading, movies } = this.state;
-    if(loading) return <div>Loading...</div>;
+
+    if( loading ) return <div>Loading...</div>;
 
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="Search..." value={this.state.title} onChange={this.handleChange}/>
+          <input type="text" placeholder="Search..." value={this.state.title} onChange={({target}) => this.handleChange(target)}/>
           <input type="submit" value="Submit"/>
         </form>
+        <div>
+        <PagingButton label="Prev Page" incr={-1} 
+          onClick={this.handlePageChange.bind(this)}
+        />
+        <PagingButton label="Next Page" incr={1} 
+          onClick={this.handlePageChange.bind(this)}
+        />
+        <Movies movies={movies}/>
+        </div>
       </div>
     )
   }
 }
 
+function PagingButton({ onClick, incr, label }) {
+  return (
+    <button onClick={() => onClick(incr)}>
+      {label}
+    </button>
+  );
+}
+
+function SubmitForm({onclick, value}) {
+  return (
+    <button>
+    {value}
+    </button>
+  )
+}
+  
 export default App;
